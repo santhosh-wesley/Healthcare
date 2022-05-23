3a38372524c310f60aaa0b44a2247a81ae7d4388
quantaStart <68285146+quantaStart@users.noreply.github.com>
2022-05-23 17:50:59 +0530

    added final

---
+++ Final/userlogin/login/models.py
@@ -0,0 +1,21 @@
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+
+
+class Admin(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+
+
+  
